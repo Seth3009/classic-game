119256c71ec07b415e089c9affc311ecd9860c58
Setiadi <setiadi.seth@gmail.com>
2022-05-25 10:18:18 +0700

    Create computerPlay Function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # classic-game
+# This is a classic rock paper scissors game, made to complete the first javascript project
---
+++ javascript.js
@@ -0,0 +1,6 @@
+function computerPlay(){
+    const num = Math.floor(Math.random()*3); 
+    const arr = ['Rock','Paper','Scissor'];
+    return arr[num];
+}
+
